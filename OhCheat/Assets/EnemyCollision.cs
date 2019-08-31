5f348dd689dcd7fae00736a408d859c8ac07f6d2
Jonathan Hoschkara <jonathan.hoschkara@wooga.net>
2019-08-31 21:09:04 +0200

    final level

---
+++ OhCheat/Assets/EnemyCollision.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Enemy")) {
+            GameManager.gameManager.Restart();
+        }
+    }
+}
