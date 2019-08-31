e95a522d885bb51da81f325b5f226db9db00c6b3
Jonathan Hoschkara <jonathan.hoschkara@wooga.net>
2019-08-31 15:26:14 +0200

    added assets

---
+++ OhCheat/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float jumpForce;
+    
+    private Rigidbody2D rb;
+
+    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(Vector2.up * jumpForce);    
+        }
+    }
+}
