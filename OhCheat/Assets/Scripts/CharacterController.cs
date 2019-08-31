cae9c0731979b69e6c871b67eeaaa1fb7b38a7df
Jonathan Hoschkara <jonathan.hoschkara@wooga.net>
2019-08-31 16:01:02 +0200

    basic jump

--- OhCheat/Assets/Scripts/CharacterController.cs
+++ OhCheat/Assets/Scripts/CharacterController.cs
@@ -1,23 +1,26 @@
 ﻿using System;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class CharacterController : MonoBehaviour
 {
     public float jumpForce;
+    public float floatHeight = 0.1f;
     
     private Rigidbody2D rb;
+    private bool canJump;
 
-    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            rb.AddForce(Vector2.up * jumpForce);    
+            rb.AddForce(Vector2.up * jumpForce);
         }
     }
 }
