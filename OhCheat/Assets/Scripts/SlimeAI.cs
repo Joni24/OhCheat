b52aaf41d202263de3103868d498f393ce6e67de
Zuramm <opheder@gmail.com>
2019-08-31 20:05:22 +0200

    Slime

---
+++ OhCheat/Assets/Scripts/SlimeAI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeAI : MonoBehaviour
+{
+    private bool facingRight = false;
+    public float speed = 2f;
+    public float runningDistance = 3f;
+    private float distanceLeft;
+
+    private new Rigidbody2D rigidbody;
+    private new SpriteRenderer renderer;
+
+    public bool FacingRight {
+        get
+        {
+            return renderer.flipX;
+        }
+        set
+        {
+            renderer.flipX = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+
+        distanceLeft = runningDistance / 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceLeft -= rigidbody.velocity.magnitude * Time.deltaTime;
+
+        if (distanceLeft < 0)
+        {
+            distanceLeft = runningDistance;
+            facingRight = !facingRight;
+        }
+
+        rigidbody.velocity = new Vector2(facingRight ? -speed : speed, rigidbody.velocity.y);
+    }
+}
