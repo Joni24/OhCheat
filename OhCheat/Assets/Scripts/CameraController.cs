2033dd187f305604d9c4ca62ff2244fe10296110
Jonathan Hoschkara <jonathan.hoschkara@wooga.net>
2019-08-31 16:44:44 +0200

    auto scroller check

---
+++ OhCheat/Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float speed = 2f;
+    
+    private void Update()
+    {
+        transform.position += Time.deltaTime * speed * Vector3.right;
+        characterController.transform.position = new Vector3(transform.position.x, characterController.transform.position.y,0f);
+    }
+}
