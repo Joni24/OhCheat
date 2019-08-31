62f9b1a68a9b0cb3206572ec96d36df8dba752e6
Jonathan Hoschkara <jonathan.hoschkara@wooga.net>
2019-08-31 18:56:54 +0200

    assigned characterRB and buttons

--- OhCheat/Assets/Scripts/CheatController.cs
+++ OhCheat/Assets/Scripts/CheatController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CheatController : MonoBehaviour
 {
@@ -24,6 +22,7 @@ public class CheatController : MonoBehaviour
             {
                 MonoBehaviour behav = (MonoBehaviour)GetComponent(cheat.script);
                 behav.enabled = !behav.enabled;
+                GameManager.gameManager.buttonDisplay.DisplayCurrentButton(cheat.key);
             }
         }
     }
