797d00394a574af84a2c1ca54399acedf7b3c58f
Jonathan Hoschkara <jonathan.hoschkara@wooga.net>
2019-08-31 17:46:10 +0200

    added ui and button color logic

---
+++ OhCheat/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = this;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
